2c76c5a1e2b671ce329be41eb7ed80852bebd6f8
pai <pai@192.168.116.140>
2013-03-08 11:27:06 +0700

    ทดสอบครั้งที่1 By punnawat

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       echo 'pai1_test_git_ver1';
+        ?>
+    </body>
+</html>
